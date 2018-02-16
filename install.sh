32d3d09a1e3b28df9ef1cefef75a157467fa5d9a
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2018-02-16 22:06:51 +0000

    Add script that copies stuff to home directory

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This has been shamelessly copied from mathiasbynens/dotfiles
+# https://github.com/mathiasbynens/dotfiles/blob/b22c32290e1518c0f228afba254ee6a3f6ab6d7a/bootstrap.sh
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
