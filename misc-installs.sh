d2169bb1b86ef1bcd7d48839349c915c5425ad45
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2019-02-16 21:01:16 +0000

    fix misc-installs.sh to use correct zsh custom directory

--- misc-installs.sh
+++ misc-installs.sh
@@ -3,11 +3,10 @@
 
 # This installs the spaceship theme for zsh
 # https://github.com/denysdovhan/spaceship-prompt
-if [ -d "{$ZSH_CUSTOM}/themes/spaceship-prompt"]
+if [ -d "{$ZSH_CUSTOM}/themes/spaceship-prompt" ]
 then
     echo "spaceship-prompt is already installed, skipping..."
 else
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 fi
-
