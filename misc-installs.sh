6685ddaba914a394d8e93c25abfaed5995aee1c5
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2019-02-16 21:22:06 +0000

    fix symlink path for spaceship prompt

--- misc-installs.sh
+++ misc-installs.sh
@@ -8,7 +8,7 @@ then
     echo "spaceship-prompt is already installed, skipping..."
 else
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+    ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 fi
 
 # Installing rustup (for managing Rust)
