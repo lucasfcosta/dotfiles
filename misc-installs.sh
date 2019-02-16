233e25d74115daea5965039580e5724910056d78
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2019-02-16 21:03:13 +0000

    add rustup installation

--- misc-installs.sh
+++ misc-installs.sh
@@ -10,3 +10,6 @@ else
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
     ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 fi
+
+# Installing rustup (for managing Rust)
+curl https://sh.rustup.rs -sSf | sh
