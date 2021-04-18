10c9306df72cbe3991f5dc529fe2622564592958
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2021-04-18 17:46:19 +0100

    install nvm and node

--- programs.sh
+++ programs.sh
@@ -53,6 +53,12 @@ brew install git
 brew install tmux
 brew install alacritty
 
+# Install nvm and Node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+
 
 # -------------------------------
 # Casks
