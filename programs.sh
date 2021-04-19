89f648dc9fc27b3cd2e4c20b8e8edf9b97b46f48
Lucas Fernandes da Costa <lucas@lucasfcosta.com>
2021-04-19 18:16:30 +0100

    install reattach-to-user-namespace so that I can open tmux

--- programs.sh
+++ programs.sh
@@ -60,6 +60,8 @@ brew install git
 brew install tmux
 brew install alacritty
 
+arch -arm64 brew install reattach-to-user-namespace
+
 # Install nvm and Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
